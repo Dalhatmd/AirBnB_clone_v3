00e6840610e6cf15c6bf2aafc9e1b9a27ac6d364
Dalhatmd <umarwalid4055@gmail.com>
2024-05-27 12:24:37 +0100

    add docs

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" city views"""
 from api.v1.views import app_views
 from models.city import City
 from models.state import State
@@ -54,6 +55,7 @@ def create_city(state_id):
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
+    """ updates city by id """
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
